05001cd6749a110a29e8b5693e18e8d1e6ce1a47
dusmel <hadadus@gmail.com>
2018-12-13 10:16:43 +0200

    fixing heroku mail 1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'https://picsar-app.herokuapp.com'}
+  config.action_mailer.default_url_options = { :host => 'picsar-app.herokuapp.com', :protocol => 'https'}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.

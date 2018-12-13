61f6b8c2da13a364a16c2c7e4c7ca216864f1ee5
dusmel <hadadus@gmail.com>
2018-12-13 10:43:37 +0200

    fixing heroku mail 3

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV["SENDGRID_USERNAME"],
   :password => ENV["SENDGRID_API_KEY"],
-  :domain => 'heroku.com',
+  :domain => 'melliom.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
